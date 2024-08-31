6d7b3af31d9ef4c24910e9c9ccad00f218daa974
Adarsh Gupta <blueadarsh1@gmail.com>
2024-08-31 10:24:13 +0530

    pus

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'ai-art-generator-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/styles.css',
+  '/static/js/main.js',
+  '/static/images/icon-192x192.png',
+  '/static/images/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
